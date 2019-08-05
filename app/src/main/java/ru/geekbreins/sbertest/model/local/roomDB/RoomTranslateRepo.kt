27b744825a1c0ba9b1e51e8443fef319c983a83b
dr-troyNIcK <dr-troyNIcK@yandex.ru>
2019-08-05 18:41:13 +0300

    add room DB

--- app/src/main/java/ru/geekbreins/sbertest/model/local/roomDB/RoomTranslateRepo.kt
+++ app/src/main/java/ru/geekbreins/sbertest/model/local/roomDB/RoomTranslateRepo.kt
@@ -1,18 +1,21 @@
 package ru.geekbreins.sbertest.model.local.roomDB
 
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 class RoomTranslateRepo {
     fun insertTranslateEntity(roomTranslateEntity: RoomTranslateEntity) {
         RoomApplicationDB.getInstance()?.roomTranslateDAO()?.insert(roomTranslateEntity)
     }
 
-    fun getAllTranslateEntities(): List<RoomTranslateEntity> {
-        Single.create<String> { }
-        return ArrayList<RoomTranslateEntity>()
+    fun getAllTranslateEntities(): Single<List<RoomTranslateEntity>> {
+        return Single.create<List<RoomTranslateEntity>> { emitter ->
+            RoomApplicationDB.getInstance()?.roomTranslateDAO()?.getAllTranslateEntities()
+                ?.let { emitter.onSuccess(it) }
+        }.subscribeOn(Schedulers.io())
     }
 
     fun deleteAllTranslateEntities() {
-
+        RoomApplicationDB.getInstance()?.roomTranslateDAO()?.deleteAllTranslateEntities()
     }
 }
